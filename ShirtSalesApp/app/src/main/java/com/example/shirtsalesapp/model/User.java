d42812a9b9627a6fa2c1230c7f25f3fd9fffbd4c
Miraitowa79 <134464103+Miraitowa79@users.noreply.github.com>
2024-07-05 23:52:40 +0700

    Login + Register Function

---
+++ ShirtSalesApp/app/src/main/java/com/example/shirtsalesapp/model/User.java
@@ -0,0 +1,94 @@
+package com.example.shirtsalesapp.model;
+
+public class User {
+    private int id;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String userName;
+    private String password;
+    private String address;
+    private String avatar;
+    private Integer status;
+    private Integer roleId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+}
